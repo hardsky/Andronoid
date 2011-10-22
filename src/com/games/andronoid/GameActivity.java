2d84939e72ac9e3397d9db3ab1bc9533b6d94232
Nikolay Mikhaylov <hardsky@yandex.ru>
2011-10-22 12:17:25 +0400

    resolve issue with home button (application crashed, when push 'Home', than again launch application

--- src/com/games/andronoid/GameActivity.java
+++ src/com/games/andronoid/GameActivity.java
@@ -1,8 +1,12 @@
 package com.games.andronoid;
 
+import java.io.FileDescriptor;
+import java.io.IOException;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.PowerManager;
 import android.os.PowerManager.WakeLock;
@@ -13,12 +17,18 @@ public class GameActivity extends Activity {
     private PowerManager mPowerManager;
     private WakeLock mWakeLock;
     private GameView mGameView;
+    private MediaPlayer mPlayer;
     
 	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
+                
+    }
+    
+    @Override
+    protected void onResume() {
+        super.onResume();
         mPowerManager = (PowerManager) getSystemService(POWER_SERVICE);
         mWakeLock = mPowerManager.newWakeLock(PowerManager.SCREEN_BRIGHT_WAKE_LOCK, getClass()
                 .getName());
@@ -39,19 +49,32 @@ public class GameActivity extends Activity {
         
         mGameView = new GameView(this, sMosaicName, sBackGroundFile, sMusicFile, nFriction, nMass, bMusic, sDifficulty);
         setContentView(mGameView);
-        
-    }
-    
-    @Override
-    protected void onResume() {
-        super.onResume();
         /*
          * when the activity is resumed, we acquire a wake-lock so that the
          * screen stays on, since the user will likely not be fiddling with the
          * screen or buttons.
          */
         mWakeLock.acquire();
-
+        /*
+        MediaPlayer mediaPlayer = new MediaPlayer(); //MediaPlayer.create(this,getAssets().open("track.mp3"));
+        try {
+        	FileDescriptor fd = new FileDescriptor();
+        	
+			mediaPlayer.setDataSource(fd);//.setDataSource("track.mp3");
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalStateException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}*/
+        
+        mPlayer = MediaPlayer.create(this, R.raw.kick);        
+        mPlayer.setLooping(true);
+        mPlayer.start();
 		mGameView.startGame();
     }
 
@@ -63,6 +86,7 @@ public class GameActivity extends Activity {
          * release our sensor resources and wake locks
          */
 
+        mPlayer.stop();
         // Stop the simulation
         mGameView.stopGame();
 
